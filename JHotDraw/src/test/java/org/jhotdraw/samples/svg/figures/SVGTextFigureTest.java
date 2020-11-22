de5fc6ae127687397e0f7823612c2b41820c5d53
Nick Bugge <nibug18@student.sdu.dk>
2020-11-22 15:55:59 +0100

    Added simple JUnit test

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGTextFigureTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.figures;
+
+import java.awt.font.TextAttribute;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nick
+ */
+public class SVGTextFigureTest {
+    
+    public SVGTextFigureTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of drawText method, of class SVGTextFigure.
+     */
+    @Test
+    public void testDrawText() {
+        System.out.println("Check for underline");
+        SVGTextFigure svg = new SVGTextFigure();
+        assertNotNull(svg);
+        assertTrue(TextAttribute.FONT.UNDERLINE_ON == 0);
+    }
+}
